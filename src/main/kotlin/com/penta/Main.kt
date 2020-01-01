a5362ede4d455f67bf9e5af486691652c36033f6
Penta <preuvinou@gmail.com>
2020-01-01 10:37:49 +0100

    Typing working

---
+++ src/main/kotlin/com/penta/Main.kt
@@ -0,0 +1,15 @@
+package com.penta
+
+import com.penta.views.AutoTyperApp
+import javafx.application.Application
+
+class Main {
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            Application.launch(AutoTyperApp::class.java)
+        }
+    }
+}
